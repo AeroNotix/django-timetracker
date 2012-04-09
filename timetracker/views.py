1be6cf5a029daf9ee9e863ef947cbd79c34c1e68
Aaron France <aaron.l.france@gmail.com>
2012-04-09 22:28:56 +0200

    Additional view preparing for a POST redirector

--- timetracker/views.py
+++ timetracker/views.py
@@ -6,6 +6,7 @@ from django.template import RequestContext
 
 from tracker.models import TrackingEntry, Tbluser
 from utils.calendar_utils import gen_calendar
+from tracker.forms import entry_form, add_form
 
 def index(request):
     return render_to_response('base.html',
@@ -21,8 +22,7 @@ def view_calendar(request,
 
     """
     Generates a calendar based on the URL it receives.
-    
-    urls are something like: www.site.com/calendar/2012/02/, also takes a day
+    site.com/calendar/2012/02/, also takes a day
     just in case you want to add a particular view for a day, for example.
 
     Even the generated HTML is aligned and pretty printed.
@@ -30,11 +30,25 @@ def view_calendar(request,
     
     calendar_table = gen_calendar(year, month, day,
                                   user='aaron.france@hp.com')
-    
+   
+
     return render_to_response(
         'calendar.html',
         {
          'calendar': calendar_table,
+         'changeform': entry_form(),
+         'addform' : add_form()
         },
         RequestContext(request)
         )
+
+
+def process_change_request(request):
+
+    """
+    Processes a change into the database from the calendar page
+    """
+    pass
+
+
+
