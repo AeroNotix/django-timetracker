0c9bd9ca6069223351f9066a085ce5e127e148ea
AeroNotix <aaron.l.france@gmail.com>
2012-04-13 13:25:18 +0200

    Ajax request now has error checking/handling and json response

--- timetracker/views.py
+++ timetracker/views.py
@@ -4,12 +4,15 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 from django.template import RequestContext
+from django.db import IntegrityError
 
 import simplejson
 
 from tracker.models import TrackingEntry, Tbluser
 from utils.calendar_utils import gen_calendar
 from tracker.forms import entry_form, add_form
+
+DUPLICATE_ENTRY = 1062
 
 def index(request):
     return render_to_response('base.html',
@@ -30,10 +33,10 @@ def view_calendar(request,
 
     The generated HTML is pretty printed
     """
-    
+
     calendar_table = gen_calendar(year, month, day,
                                   user='aaron.france@hp.com')
-   
+
 
     return render_to_response(
         'calendar.html',
@@ -56,6 +59,8 @@ def process_change_request(request):
 
 def ajax(request):
 
+    error = ''
+
     if not request.is_ajax():
         raise Http404
 
@@ -69,12 +74,60 @@ def ajax(request):
     for key in form:
         form[key] = request.POST.get(key, None)
 
+
+    json_data = {
+        "success": False,
+        "error": "",
+        "calendar": ""
+        }
+        
+    shour, sminute = map(int,
+                         form['start_time'].split(":")
+                     )
+
+    ehour, eminute = map(int,
+                         form['end_time'].split(":")
+                     )
+
+    if (datetime.time(shour, sminute) > datetime.time(ehour, eminute)):
+        json_data['error'] = "Start time after end time"
+        return HttpResponse(simplejson.dumps(json_data),
+                            mimetype="application/javascript")
+
+        
+        
     # need to use sessions
     form['user_id'] = 1
+    # need to add a breaks section to the form
     form['breaks'] = "00:15:00"
-
+      
     # lol
-    entry = TrackingEntry(**form)
-    entry.save()
+    try:
+        entry = TrackingEntry(**form)
+        entry.save()
+        
+        year, month, day = map(int,
+                               form['entry_date'].split("-")
+                           )
 
-    return HttpResponse(e)
+        calendar = gen_calendar(year, month, day,
+                                user='aaron.france@hp.com')
+        
+    except IntegrityError as error:
+        if error[0] == DUPLICATE_ENTRY:
+
+            json_data['error'] = "There is a duplicate entry for this value"
+            
+            return HttpResponse(simplejson.dumps(json_data),
+                                mimetype="application/javascript")
+        else:
+            json_data['error'] = str(error)
+            return HttpResponse(simplejson.dumps(json_data),
+                                mimetype="application/javascript")
+        
+    # if all went well
+    json_data['success'] = True
+    json_data['calendar'] = calendar
+    return HttpResponse(simplejson.dumps(json_data),
+                        mimetype="application/javascript")
+    
