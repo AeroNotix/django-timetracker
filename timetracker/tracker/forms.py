ae5ca73a70b7135372b43d35012562373d170a18
Aaron France <aaron.l.france@gmail.com>
2012-04-15 14:02:33 +0200

    Changes to the form naming convention. This brings around a possibility Now it will be possible to move from two duplicated forms to a single, form class which takes an argument for the form's id.

--- timetracker/tracker/forms.py
+++ timetracker/tracker/forms.py
@@ -13,30 +13,28 @@ class entry_form(forms.Form):
     Change entry form
     """
 
-
     entry_date = forms.DateField(label="Entry Date:")
     entry_date.widget.attrs.update({'class': 'change-el',
-                                    'id': 'line_entrydate',
+                                    'id': 'change_entrydate',
                                     'readonly': 'readonly'})
 
     start_time = forms.TimeField(label="Start Time:")
     start_time.widget.attrs.update({'class': 'change-el',
-                                    'id': 'line_starttime'})
+                                    'id': 'change_starttime'})
 
     end_time = forms.TimeField(label="End Time:")
     end_time.widget.attrs.update({'class': 'change-el',
-                                    'id': 'line_endtime'})
+                                    'id': 'change_endtime'})
 
     daytype = forms.ChoiceField(label="Day Type:", choices=DAYTYPE_CHOICES)
     daytype.widget.attrs.update({'class': 'change-el',
-                                    'id': 'cmb_daytype'})
+                                    'id': 'change_daytype'})
     
     
 class add_form(forms.Form):
     """
     Add entry form
     """
-
 
     entry_date = forms.DateField(label="Entry Date:")
     entry_date.widget.attrs.update({'class': 'change-el',
