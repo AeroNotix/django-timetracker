179073c77fdfea3730e1c84b4dca0018cf948d81
Aaron France <aaron.l.france@gmail.com>
2012-04-15 17:43:26 +0200

    Added login/login mapping

--- timetracker/urls.py
+++ timetracker/urls.py
@@ -10,13 +10,15 @@ month = '(?P<month>\d{1,2})'
 day = '(?P<day>\d{1,2})'
 
 urlpatterns = patterns('',
-    url(r'^$', 'timetracker.views.index'),
+    url(r'^$', views.index),
     url(r'^calendar$', views.view_calendar),
     url(r'^calendar/$', views.view_calendar),
     url(r'^calendar/%s/?$' % year, views.view_calendar),
     url(r'^calendar/%s/%s/?$' % (year, month), views.view_calendar),
     url(r'^calendar/%s/%s/%s/?$' % (year, month, day), views.view_calendar),
     url(r'^ajax/?$', views.ajax),
+    url(r'^login/?$', views.login),
+    url(r'^logout/?$', views.logout),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
