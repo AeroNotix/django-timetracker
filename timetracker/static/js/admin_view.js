c1c1efd7b1076afb51f6a87863e35a7e2c08001e
AeroNotix <aaron.l.france@gmail.com>
2012-04-17 15:36:55 +0200

    Admin view calendar ajax function working

--- timetracker/static/js/admin_view.js
+++ timetracker/static/js/admin_view.js
@@ -4,6 +4,21 @@
 */
 
 function onOptionChange(date) {
+    $.ajaxSetup({type: 'POST'});
+
+    var eeid = $("#user_select").val();
+
+    $.ajax({
+        url: "/ajax/",
+        data: {
+            'form_type': 'admin_get',
+            'eeid': eeid
+        },
+        dataType: "json",
+        success: function (data) {
+            alert(data);
+        }
+    });
 }
     
     
