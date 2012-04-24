50c72b7bd4a4b11e6a9a12d6ebb746d35da94591
Aaron France <aaron.l.france@gmail.com>
2012-04-24 22:11:50 +0200

    Moved datemaps to it's own module to share easier

---
+++ timetracker/utils/datemaps.py
@@ -0,0 +1,14 @@
+MONTH_MAP = {
+    0: ('JAN', 'January'),
+    1: ('FEB', 'February'),
+    2: ('MAR', 'March'),
+    3: ('APR', 'April'),
+    4: ('MAY', 'May'),
+    5: ('JUN', 'June'),
+    6: ('JUL', 'July'),
+    7: ('AUG', 'August'),
+    8: ('SEP', 'September'),
+    9: ('OCT', 'October'),
+    10:('NOV', 'November'),
+    11:('DEC', 'December')
+}
