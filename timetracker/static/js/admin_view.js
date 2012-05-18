112330a7d152de27bc7d63fa2fe8b847aa9a969b
AeroNotix <aaron.l.france@gmail.com>
2012-05-18 16:52:47 +0200

    Added javascript function to insert the user's daily tracking time into a div and inserted a div into the template to receive said values

--- timetracker/static/js/admin_view.js
+++ timetracker/static/js/admin_view.js
@@ -31,6 +31,29 @@ function onOptionChange(date) {
     return true;
 }
 
+function toggleChangeEntries(st_hour, st_min, full_st,
+                             fi_hour, fi_min, full_fi,
+                             entry_date, daytype,
+                             change_id, breakLength,
+                             breakLength_full) {
+
+    var balance = fi_hour - st_hour;
+    console.log(balance);
+
+    var text_information = "Start Time: " + full_st;
+    var text_information = text_information + " | End Time: " + full_fi;
+    var text_information = text_information + " | Breaks: " + breakLength_full;
+    
+    $("#day_information").fadeTo(500, 0, function() {    
+        $("#day_information").text(text_information);
+    });
+    $("#day_information").fadeTo(500, 1);
+}
+
+function hideEntries() {
+    $("#day_information").text('');
+}
+
 $(function () {
     $("#user_select").change(function () {
         onOptionChange();
