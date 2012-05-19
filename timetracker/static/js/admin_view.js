d5cc3aea1dbc26d48f0adcf2ade6be7b3b44a98b
Aaron France <aaron.l.france@gmail.com>
2012-05-19 15:32:28 +0200

    Added clickable days onto the admin_view page, can now get info about each day

--- timetracker/static/js/admin_view.js
+++ timetracker/static/js/admin_view.js
@@ -36,13 +36,48 @@ function toggleChangeEntries(st_hour, st_min, full_st,
                              entry_date, daytype,
                              change_id, breakLength,
                              breakLength_full) {
-        
-    var text_information = "Start Time: " + full_st;
-    var text_information = text_information + " | End Time: " + full_fi;
-    var text_information = text_information + " | Breaks: " + breakLength_full;
+
+    "use strict";
+
+    var start_string = "Start Time: "+full_st;
+    var end_string = " | End Time: "+full_fi;
     
+    function pad(str, length, chr) {
+        str = '' + str;
+        if (str.length < length) {
+            var pad_string = '';
+            while ((pad_string+str).length < (length)) {
+                pad_string = chr+pad_string;
+            }
+            str = pad_string+str;
+        }
+        return str;
+    }
+
+    /*
+       Here we are building the time string from the start time,
+       the end time and the break time.
+
+       What we do is minus the start time from the end time,
+       then minus the break time in minutes from 60, then
+       concatenate them all together. This will break if the
+       break time needs to go above 60 minutes.
+    */
+
+    var break_time = 60 - breakLength;
+    if (break_time < 60) {
+        var breaks = pad(60 - breakLength, 2, "0");
+        var hour_string = pad((fi_hour - st_hour) - 1, 2, "0");
+    } else {
+        var breaks = "00";
+        var hour_string = pad(fi_hour - st_hour, 2, "0");
+    }
+    
+    var shiftLength = " | Shift Length: " + hour_string + ":" + breaks;
+
+    var text_information = start_string+end_string;
     $("#day_information").fadeTo(500, 0, function() {    
-        $("#day_information").text(text_information);
+        $("#day_information").text(text_information + shiftLength);
     });
     $("#day_information").fadeTo(500, 1);
 }
