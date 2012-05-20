4954cfe5745d5103a94c70b6f030320add5d3bdd
Aaron France <aaron.l.france@gmail.com>
2012-05-20 12:25:54 +0200

    Added import hack to detect which settings module we're running from

--- loggers.py
+++ loggers.py
@@ -4,15 +4,21 @@ A module to create and share a few logging instances
 
 import logging
 import os
-try:
-    from settings import ROOT_LOG_DIR
-except ImportError:
-    ROOT_LOG_DIR = os.path.dirname(__file__)
-try:
-    from settings import LOGLEVEL
-except ImportError:
-    LOGLEVEL = logging.DEBUG
 
+'''
+Hacky method of importing the settings module that is currently
+running.
+
+The way this works is by detecting the environment setting for
+the settings module which Django loads (we load it in the case
+of apache) and then uses that setting as the string name, we
+then build a list of module level variable names which we want
+to import, set up both dicts to insert them from/to then put it
+into the name space of _mod_.
+'''
+settings_module = os.environ.get("DJANGO_SETTINGS_MODULE")
+fromset = ["ROOT_LOG_DIR", "LOGLEVEL"]
+_mod_ = __import__(settings_module, globals(), locals(), fromset, -1)
 
 def create_logger(filename,
                   level=logging.DEBUG,
@@ -25,18 +31,18 @@ def create_logger(filename,
     )
 
     logger = logging.getLogger(filename)
-    logger.setLevel(LOGLEVEL)
+    logger.setLevel(_mod_.LOGLEVEL)
     fh = logging.FileHandler(os.path.join(root_path, filename + '.log'))
     fh.setFormatter(frmt)
     logger.addHandler(fh)
     return logger
 
-database_log = create_logger('database', root_path=ROOT_LOG_DIR)
-email_log = create_logger('email', root_path=ROOT_LOG_DIR)
-debug_log = create_logger('debug', root_path=ROOT_LOG_DIR)
-info_log = create_logger('info', root_path=ROOT_LOG_DIR)
-error_log = create_logger('error', root_path=ROOT_LOG_DIR)
-suspicious_log = create_logger('suspicious', root_path=ROOT_LOG_DIR)
+database_log = create_logger('database', root_path=_mod_.ROOT_LOG_DIR)
+email_log = create_logger('email', root_path=_mod_.ROOT_LOG_DIR)
+debug_log = create_logger('debug', root_path=_mod_.ROOT_LOG_DIR)
+info_log = create_logger('info', root_path=_mod_.ROOT_LOG_DIR)
+error_log = create_logger('error', root_path=_mod_.ROOT_LOG_DIR)
+suspicious_log = create_logger('suspicious', root_path=_mod_.ROOT_LOG_DIR)
 
 if __name__ == '__main__':
     # test the logs
