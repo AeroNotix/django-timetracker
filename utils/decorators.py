77a32c2f1d2eecc93585b842e0e1066163869372
AeroNotix <aaron.l.france@gmail.com>
2012-05-21 16:50:45 +0200

    Documented decorator module

--- utils/decorators.py
+++ utils/decorators.py
@@ -14,14 +14,22 @@ from timetracker.loggers import (debug_log, info_log,
 
 def loggedin(func):
 
-    """
-    Decorator to make sure that the view is being accessed
-    by a logged in user
+    """Decorator to make sure that the view is being accessed by a
+    logged in user.
+
+    This works by simply checking that the user_id in the session
+    table is 1) There and 2) A real user. If either of these aren't
+    satisfied we throw back a 404.
+
+    We also log this.
+
+    :param func: A function which has a request object as a parameter
+    :returns: Nothing directly, it returns the function it decorates
+    :raises: :class:`Http404` error
     """
 
     @wraps(func)
     def inner(request, *args, **kwargs):
-
         try:
             Tbluser.objects.get(id=request.session.get("user_id"))
         except Tbluser.DoesNotExist:
@@ -33,8 +41,15 @@ def loggedin(func):
 
 def admin_check(func):
 
-    """
-    Wrapper to see if the view is being called as an admin
+    """Wrapper to see if the view is being called as an admin
+
+    This works by 1) Checking if there is a user_id in the session
+    table. 2) If that user is a real user in the database and 3) if
+    that user's is_admin() returns True.
+
+    :param func: A function with a request object as a parameter
+    :returns: Nothing directly, it returns the function it decorates.
+    :raises: :class:`Http404` error
     """
 
     @wraps(func)
@@ -65,6 +80,9 @@ def json_response(func):
     This is useful because the call site can literally just
     call the function as it is without needed to make a http-
     response.
+
+    :param func: Function which returns a dictionary
+    :returns: :class:`HttpResponse` with mime/application as JSON
     """
 
     @wraps(func)
@@ -82,17 +100,25 @@ def json_response(func):
 
 def request_check(func):
 
-    """
-    Decorator to check an incoming request against a few rules
+    """Decorator to check an incoming request against a few rules
+
+    This function should decorate any function which is supposed
+    to be accessed by and only by Ajax. If we see that the function
+    was accessed by any other means, we raise a :class:`Http404`
+    and give up processing the page.
+
+    We also make a redundant check to see if the user is logged in.
+
+    :param func: Function which has a request object parameter.
+    :returns: The function which it decorates.
+    :raises: :class:`Http404`
     """
 
     @wraps(func)
     def inner(request):
-
         """
         Pulls the request object off the decorated function
         """
-
         if not request.is_ajax():
             suspicious_log.info("Request check made on non-ajax call")
             raise Http404
