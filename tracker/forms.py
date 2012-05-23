92b729c2284163120fdf0b60fce42ceb6a57df5d
Aaron France <aaron.l.france@gmail.com>
2012-05-23 19:48:06 +0200

    Added further sphinx-style docs to tracker.forms

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,5 +1,18 @@
-'''
-Forms used for user input for the tracker app
+'''Forms used for user input for the tracker app
+
+Several forms are extremely simple and thus it's not required to 'hard-code'
+them into HTML templates, which would couple our output with the view
+functions which simply isn't good MVC practice.
+
+Module Overview
++++++++++++++++
+
+=========================  ========================
+..                         ..
+=========================  ========================
+:class:`EntryForm`         :class:`AddForm`
+:class:`Login`
+=========================  ========================
 '''
 
 from django import forms
@@ -7,8 +20,15 @@ from timetracker.utils.datemaps import DAYTYPE_CHOICES
 
 
 class EntryForm(forms.Form):
-    """
-    Change entry form
+    """Change entry form
+
+    This Class creates a form which allows users to change an existing entry
+    which they have added to the timetracking portion of the site. The fields
+    match up precisely to the :class:`AddForm` because the data will be the
+    same. We have duplicated the code here because we need to explicitly set
+    the id values using the widget.attrs.update on the widget dict. This is so
+    that each widget could be styled individually and so that we have
+    something easily accessbile by front-end javascript code.
     """
 
     entry_date = forms.DateField(label="Entry Date:")
@@ -37,8 +57,11 @@ class EntryForm(forms.Form):
 
 
 class AddForm(forms.Form):
-    """
-    Add entry form
+    """Add entry form
+
+    This Class creates a form which allows users to add an entry into the
+    timetracking portion of the app. See :class:`ChangeEntry` for a detailed
+    description of these two classes as they have a high coupling factor.
     """
 
     entry_date = forms.DateField(label="Entry Date:")
@@ -68,8 +91,13 @@ class AddForm(forms.Form):
 
 class Login(forms.Form):
 
-    """
-    Basic login form
+    """ Basic login form
+
+    This form renders into a very simple login field, with fields identified
+    differently, both for styling and the optional javascript by-name
+    handling. Whilst this form is extremely simple, coding one each and
+    every time we wish to use one is just dumb and it's much easier to code it
+    once and import it into our project.
     """
 
     user_name = forms.CharField(
