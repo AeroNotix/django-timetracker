7d9e7137bc82180483734964dbd46847b20ea7e1
AeroNotix <aaron.l.france@gmail.com>
2013-01-09 14:33:55 +0100

    We don't need the hidden is_team_leader div.

--- utils/datemaps.py
+++ utils/datemaps.py
@@ -105,8 +105,6 @@ def generate_year_box(year, id=''):
 def generate_employee_box(admin_user):
     from timetracker.tracker.models import Tblauthorization as tblauth
     admin_user = admin_user.get_administrator()
-    is_team_leader = admin_user.is_tl()
-    is_admin = admin_user.is_admin()
     ees = admin_user.get_subordinates()
     ees_tuple = [(user.id, user.name()) for user in ees]
     ees_tuple.append(("null", "----------"))
