21ae770242aa2758a08c3e892c58410b652cb783
AeroNotix <aaron.l.france@gmail.com>
2013-02-13 15:24:50 +0100

    Moved towards the production style code for the end-of-month email notifications. Debugging must use the console SMTP server as to not send out a tonne of e-mails whilst testing.

--- tracker/management/commands/notifyovertime.py
+++ tracker/management/commands/notifyovertime.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 from timetracker.tracker.models import *
 
+
 class Command(BaseCommand):
     args = '<poll_id poll_id ...>'
     help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
-        for user in Tbluser.objects.filter(id=1):
+        for user in Tbluser.objects.filter(disabled=False):
+            if user.get_total_balance(ret='num') <= 0:
+                continue
             user.send_pending_overtime_notification()
-            break
