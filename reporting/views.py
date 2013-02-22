8a056aeb33eb1db4f853a6a684e29f970740e17d
AeroNotix <aaron.l.france@gmail.com>
2013-02-22 16:44:15 +0100

    Finished up the ot_by_year/month functions.

--- reporting/views.py
+++ reporting/views.py
@@ -13,7 +13,7 @@ from django.http import HttpResponse, Http404
 from timetracker.utils.decorators import admin_check, loggedin
 from timetracker.tracker.models import Tbluser, TrackingEntry
 from timetracker.tracker.models import Tblauthorization as tblauth
-from timetracker.utils.datemaps import generate_employee_box, generate_month_box
+from timetracker.utils.datemaps import generate_employee_box, generate_month_box, MONTH_MAP
 from timetracker.utils.writers import UnicodeWriter
 
 @admin_check
@@ -74,8 +74,48 @@ def yearmonthhol(request, year=None, month=None):
 
 @admin_check
 def ot_by_month(request, year=None, month=None):
-    return HttpResponse("Sup")
+    auth_user = Tbluser.objects.get(id=request.session.get("user_id"))
+    buf = StringIO()
+    buf.write("\xef\xbb\xbf")
+    csvfile = UnicodeWriter(buf)
+    csvfile.writerow(
+        ["Name", "Team", MONTH_MAP[int(month)-1][1]]
+        )
+    total_balance = 0
+    for user in auth_user.get_subordinates():
+        balance = user.get_total_balance(ret='flo', year=year, month=month)
+        total_balance += balance
+        csvfile.writerow([user.name(), user.process, "%.2f" % balance])
+    csvfile.writerow(["Total", "Total", "%.2f" % total_balance])
+    response = HttpResponse(buf.getvalue(), mimetype="text/csv")
+    response['Content-Disposition'] = \
+        'attachment;filename=OT_By_Month_%s_%s.csv' % (year, month)
+    return response
 
 @admin_check
 def ot_by_year(request, year=None):
-    return HttpResponse("Yo")
+    auth_user = Tbluser.objects.get(id=request.session.get("user_id"))
+    buf = StringIO()
+    buf.write("\xef\xbb\xbf")
+    csvfile = UnicodeWriter(buf)
+    csvfile.writerow(
+        ["Name", "Team"] + [MONTH_MAP[n][1] for n in range(0,12)]
+        )
+    total_balance = 0
+    balances = {
+        n: 0 for n in range(1, 13)
+        }
+    for user in auth_user.get_subordinates():
+        row = [user.name(), user.process]
+        for month in range(1, 13):
+            balance = user.get_total_balance(ret='flo', year=year, month=month)
+            balances[month] += balance
+            row.append("%.2f" % balance if balance != 0.0 else "-")
+        csvfile.writerow(row)
+    totalrow = ["Total", "Total"]
+    [totalrow.append("%.2f" % balances[n]) for n in range(1,13)]
+    csvfile.writerow(totalrow)
+    response = HttpResponse(buf.getvalue(), mimetype="text/csv")
+    response['Content-Disposition'] = \
+        'attachment;filename=OT_By_Month_%s_%s.csv' % (year, month)
+    return response
