3fad382e7a0ac8afb586d32cddb6889cf7806356
AeroNotix <aaron.l.france@gmail.com>
2013-02-27 11:40:16 +0100

    Fixed row highlight for IE.

--- static/js/holiday_shared.js
+++ static/js/holiday_shared.js
@@ -1,19 +1,13 @@
 var previous = 0;
-var previous_css = '';
 function highlight_row(Row) {
     if (previous) {
-        $("#" + previous + "_row").css({
-            "border": previous_css
-        });
+        $("#" + previous + "_row").removeClass("highlighted-row")
         /* We need to trigger a redraw because WebKit browsers sometimes
            fail to flush the layout changes. */
         $("#" + previous + "_row").hide(0, function() {
             $("#" + previous + "_row").show();
         });
     }
-    previous_css  = $("#" + Row + "_row").css("border");
-    $("#" + Row + "_row").css({
-        "border": "3px solid black"
-    });
+    $("#" + Row + "_row").addClass("highlighted-row");
     previous = Row;
 }
