3e6407b5b4a3b8c769abdfd6b64c1c7393f2b0c3
AeroNotix <aaron.l.france@gmail.com>
2013-02-28 15:56:13 +0100

    Reorganized code.

--- tracker/models.py
+++ tracker/models.py
@@ -872,6 +872,14 @@ class TrackingEntry(models.Model):
         unique_together = ('user', 'entry_date')
         ordering = ['user']
 
+    def save(self, *args, **kwargs):
+        super(TrackingEntry, self).save(*args, **kwargs)
+        self.full_clean()
+        if self.daytype == "WKDAY" and \
+                self.entry_date.isoweekday() in [6, 7]:
+            self.daytype = "SATUR"
+            super(TrackingEntry, self).save(*args, **kwargs)
+
     def __unicode__(self):
 
         '''
@@ -915,14 +923,6 @@ class TrackingEntry(models.Model):
             self.end_time, self.breaks, self.get_daytype_display(),
             self.comments
             ]
-
-    def save(self, *args, **kwargs):
-        super(TrackingEntry, self).save(*args, **kwargs)
-        self.full_clean()
-        if self.daytype == "WKDAY" and \
-                self.entry_date.isoweekday() in [6, 7]:
-            self.daytype = "SATUR"
-            super(TrackingEntry, self).save(*args, **kwargs)
 
     def threshold(self):
         return settings.OT_THRESHOLDS.get(
