23b7b7d262271ef4f5a03a469a502bf49884ad26
Aaron <aaron.l.france@gmail.com>
2013-03-05 21:37:00 +0100

    JSLint'd reporting.js.

--- static/js/reporting.js
+++ static/js/reporting.js
@@ -1,14 +1,17 @@
-
+/*global $,alert,window*/
 function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+	"use strict";
+	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function overtime_data() {
+	"use strict";
     alert("Not implemented");
 }
 
 function all_holiday_data() {
-    if ($("#user_select").val() == "null") {
+	"use strict";
+    if ($("#user_select").val() === "null") {
         return;
     }
     window.location.assign(
@@ -17,13 +20,15 @@ function all_holiday_data() {
 }
 
 function yearmonth_holiday_data() {
-    var year =  $("#yearbox_hol").val()
+	"use strict";
+    var year =  $("#yearbox_hol").val();
     if (isNumber(year) && year.length >= 4) {
-        window.location.assign(
-            ["/reporting/yearmonthhol/",
-             year + "/", 
-             $("#monthbox_hol").val() + "/"].join("")
-        );
+        window.location.assign([
+			"/reporting/yearmonthhol/",
+            year + "/",
+            $("#monthbox_hol").val() + "/"
+		].join("")
+							  );
     } else {
         $("#yearbox_hol").text("");
         alert("Invalid year.");
@@ -31,13 +36,15 @@ function yearmonth_holiday_data() {
 }
 
 function ot_by_month() {
-    var year = $("#yearbox_ot_month").val()
+	"use strict";
+    var year = $("#yearbox_ot_month").val();
     if (isNumber(year) && year.length >= 4) {
-        window.location.assign(
-            ["/reporting/ot_by_month/",
-             year + "/",
-             $("#monthbox_ot").val() + "/"].join("")
-        );
+        window.location.assign([
+			"/reporting/ot_by_month/",
+            year + "/",
+            $("#monthbox_ot").val() + "/"
+		].join("")
+							  );
     } else {
         $("#yearbox_hol").text("");
         alert("Invalid year.");
@@ -45,13 +52,14 @@ function ot_by_month() {
 }
 
 function ot_by_year() {
-    var year = $("#yearbox_ot_year").val()
+	"use strict";
+    var year = $("#yearbox_ot_year").val();
     if (isNumber(year) && year.length >= 4) {
-        window.location.assign(
-            ["/reporting/ot_by_year/",
-             year + "/",
-            ].join("")
-        );
+        window.location.assign([
+			"/reporting/ot_by_year/",
+            year + "/",
+        ].join("")
+							  );
     } else {
         $("#yearbox_ot_year").text("");
         alert("Invalid year.");
@@ -59,13 +67,14 @@ function ot_by_year() {
 }
 
 function holidays_for_yearmonth() {
-    var year = $("#yearbox_hols_year").val()
+	"use strict";
+    var year = $("#yearbox_hols_year").val();
     if (isNumber(year) && year.length >= 4) {
-        window.location.assign(
-            ["/reporting/hols_for_yearmonth/",
-             year + "/"
-            ].join("")
-        );
+        window.location.assign([
+			"/reporting/hols_for_yearmonth/",
+            year + "/"
+        ].join("")
+							  );
     } else {
         $("#yearbox_hols_year").text("");
         alert("Invalid year.");
