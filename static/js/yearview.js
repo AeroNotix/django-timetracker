80b38c39bb3620b390df3f80b923e5f01e1af35c
Aaron <aaron.l.france@gmail.com>
2013-03-05 21:12:00 +0100

    JSLint'd yearview.js.

--- static/js/yearview.js
+++ static/js/yearview.js
@@ -1,11 +1,14 @@
+/*global $,window*/
 function onchanger() {
+	"use strict";
     if ($("#user_select").val() === "null") {
         return;
     }
     window.location.href = "/yearview/" + $("#user_select").val() + "/" + $("#cmb_yearbox").val() + "/";
 }
 
-$(function() {
+$(function () {
+	"use strict";
     $("#user_select").change(onchanger);
     $("#cmb_yearbox").change(onchanger);
     $("#cmb_yearbox").val($("#hidden_year").text());
