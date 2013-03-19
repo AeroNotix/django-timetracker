90983f4a3d17ac14e6603e38d8778f1c98065901
AeroNotix <aaron.l.france@gmail.com>
2013-03-19 12:38:33 +0100

    Documentated and fixed linting errors for the notifyovertime command.

--- tracker/management/commands/notifyovertime.py
+++ tracker/management/commands/notifyovertime.py
@@ -1,3 +1,6 @@
+'''This enables a django command for sending a total to an individual about
+there overtime balances.'''
+
 from django.core.management.base import BaseCommand, CommandError
 from timetracker.tracker.models import *
 
@@ -5,10 +8,12 @@ from django.core import mail
 connection = mail.get_connection()
 
 class Command(BaseCommand):
+    '''Implementation of a Django command.'''
     help = 'Sends notifications of overtime balances to the all those ' \
            'who have balances over zero hours.'
 
     def handle(self, *args, **options):
+        '''Main entry point'''
         messages = filter(lambda x: x != None,
                           [user.send_pending_overtime_notification(send=False)
                            for user in Tbluser.objects.filter(disabled=False)])
