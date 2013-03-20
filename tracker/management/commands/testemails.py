6c0eecbdcec413a1ae766e791ed188e8df563140
AeroNotix <aaron.l.france@gmail.com>
2013-03-20 13:01:00 +0100

    PyLint and documentation changes to the test e-mails command.

--- tracker/management/commands/testemails.py
+++ tracker/management/commands/testemails.py
@@ -1,10 +1,15 @@
+'''
+Simple module to aid in command-line debugging of notification related issues.
+'''
+
 from django.core.management.base import BaseCommand, CommandError
-from timetracker.tracker.models import *
+from timetracker.tracker.models import TrackingEntry
 
 class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
+    '''Django command'''
     help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
+        '''implementation'''
         for i in TrackingEntry.objects.filter(user_id=331):
             i.send_notifications()
