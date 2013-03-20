abd88be9282d48a1fbe5e811916a5eff2f8eadc2
AeroNotix <aaron.l.france@gmail.com>
2013-03-20 13:00:39 +0100

    Use FQ import names on the send_weekly_reminders.py.

--- tracker/management/commands/send_weekly_reminders.py
+++ tracker/management/commands/send_weekly_reminders.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from timetracker.tracker.models import *
+from timetracker.tracker.models import Tbluser
 
 class Command(BaseCommand):
     help = \
