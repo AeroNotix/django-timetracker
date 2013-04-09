bc2a07749bfe6a7492b1d4588eac98c3ccac6c49
AeroNotix <aaron.l.france@gmail.com>
2013-04-09 10:38:59 +0200

    Added extra SQL update step to the SQL scheme change file.

--- sqlupdate.sql
+++ sqlupdate.sql
@@ -1,2 +1,3 @@
- ALTER TABLE `tracker_trackingentry` ADD `link_id` integer UNIQUE;
- ALTER TABLE `tracker_trackingentry` ADD CONSTRAINT `link_id_refs_id_28999898` FOREIGN KEY (`link_id`) REFERENCES `tracker_trackingentry` (`id`);
+ALTER TABLE `tracker_trackingentry` ADD `link_id` integer UNIQUE;
+ALTER TABLE `tracker_trackingentry` ADD CONSTRAINT `user_id_refs_id_dc1688ef` FOREIGN KEY (`user_id`) REFERENCES `tbluser` (`id`);
+ALTER TABLE `tracker_trackingentry` ADD CONSTRAINT `link_id_refs_id_28999898` FOREIGN KEY (`link_id`) REFERENCES `tracker_trackingentry` (`id`);
