6ec8007e9ab0a70b0a6d096692330123bbeb68f3
Aaron <aaron.l.france@gmail.com>
2013-05-25 16:24:52 +0200

    Added the client-side code for the VCS section.

---
+++ static/js/vcs.js
@@ -0,0 +1,21 @@
+var $ = $ || function() {}; // Keeps from throwing ref errors.
+
+function swapselected(from, to) {
+	$(to).html(options[$(from).val()]);
+}
+
+function detailselected(from, to) {
+	$(to).text(details[$(from).val()]);
+}
+
+$(function() {
+	$("#grp-slct").change(
+		function() {
+			swapselected("#grp-slct", "#subgrp-slct");
+		});
+	$("#subgrp-slct").change(
+		function() {
+			detailselected("#subgrp-slct", "#detail-msg");
+		});
+
+});
