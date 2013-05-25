d396898e16d753a654015c0f7772b2faee1922d0
Aaron <aaron.l.france@gmail.com>
2013-05-25 17:47:03 +0200

    Handle the server-side stuff when Activity Entries are created.

--- vcs/views.py
+++ vcs/views.py
@@ -3,9 +3,30 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from timetracker.utils.decorators import loggedin
+from timetracker.vcs.models import ActivityEntry, Activity
+from timetracker.tracker.models import Tbluser
 
 @loggedin
 def vcs(request):
+    return render_to_response(
+        "vcs.html",
+        {},
+        RequestContext(request)
+    )
+
+@loggedin
+def vcs_add(request):
+    activity_key = request.POST.get("activity_key")
+    amount = request.POST.get("amount")
+    user_id = request.session.get("user_id")
+
+    if not activity_key or not amount:
+        raise Http404
+
+    activity = Activity.objects.get(id=activity_key)
+    user = Tbluser.objects.get(id=user_id)
+    ActivityEntry(user=user, activity=activity, amount=amount).save()
+
     return render_to_response(
         "vcs.html",
         {},
