d3a826be9e2a8774c52db5cedc15f6bf3c37d31a
AeroNotix <aaron.l.france@gmail.com>
2013-06-11 15:11:44 +0100

    Added the serialization of activities and enabled the 'pick a date' functionality.

--- static/js/vcs.js
+++ static/js/vcs.js
@@ -1,4 +1,5 @@
 var $ = $ || function() {}; // Keeps from throwing ref errors.
+var CONFIG = {};
 
 function swapselected(from, to) {
 	$(to).html(options[$(from).val()]);
@@ -19,4 +20,49 @@ $(function() {
 			$("#activity_key").attr("value", $("#subgrp-slct").val());
 		});
 
+    $("#which-date").change(
+        function() {
+            $.ajax({
+                method: "GET",
+                url: CONFIG.AJAX_ENTRIES_URL,
+                data: {
+                    "date": $("#which-date").val()
+                },
+                success: function (data) {
+                    var i;
+                    $("#entries")
+                        .children()
+                        .remove();
+                    $("#entries").append("<th>Type</th><th>Amount</th>");
+
+                    for (i = 0; i < data.entries.length; i++) {
+                        $("#entries").append(["<tr><td>",
+                                           data.entries[i].text,
+                                           "</td><td><input value=\"",
+                                           data.entries[i].amount,
+                                           "\"/></td></tr>"].join(""));
+                    }
+                    $("#entries").find("input").each(function() {
+                        $(this).spinner({spin:
+                                         function () {
+                                             console.log("sup");
+                                             console.log($(this).id);
+                                         },
+                                         min: 0
+                                        });
+                    });
+                },
+                error: function (a,b,c) {
+                    $("#entries")
+                        .children()
+                        .remove();
+                }
+            });
+        });
+
+    $(function() {
+        $("#amount-box").spinner({"min": 0});
+        $("#which-date").datepicker().datepicker("option", "dateFormat", "yy-mm-dd");
+        $("#which-date").datepicker("setDate", "{{ todays_date}}");
+    });
 });
