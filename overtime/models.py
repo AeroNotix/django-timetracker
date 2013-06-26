a7345ecb7acf4f07499622f1a886c50b10f3e252
AeroNotix <aaron.l.france@gmail.com>
2013-06-26 18:01:38 +0100

    Added the logic on the PendingApproval model for when the entry is approved or denied.

--- overtime/models.py
+++ overtime/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.core.mail import EmailMessage
 
 from timetracker.tracker.models import TrackingEntry, Tbluser
 
@@ -26,3 +27,26 @@ class PendingApproval(models.Model):
         self.closed = True
         self.closed_on = datetime.datetime.now()
         self.save()
+        if status:
+            self.approved()
+        else:
+            self.denied()
+
+    def approved(self):
+        self.entry.send_notifications()
+
+    def denied(self):
+        message = \
+                  "Hi,\n\n" \
+                  "Your request for overtime on %s was denied.\n\n" \
+                  "Kind Regards,\n" \
+                  "Timetracking Team"
+        message = message % str(self.entry.entry_date)
+        email = EmailMessage(from_email='timetracker@unmonitored.com')
+        email.body = message
+        email.to = [self.entry.user.user_id]
+        email.subject = "Request for Overtime: Denied."
+        email.send()
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.entry.entry_date, self.entry.user.name())
