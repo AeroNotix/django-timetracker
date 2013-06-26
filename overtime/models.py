2d7d84835210c183ca78dab5204aa10a24b94945
AeroNotix <aaron.l.france@gmail.com>
2013-06-26 14:44:25 +0100

    Added the PendingApproval model.

--- overtime/models.py
+++ overtime/models.py
@@ -1,3 +1,28 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+from timetracker.tracker.models import TrackingEntry, Tbluser
+
+
+class PendingApproval(models.Model):
+
+    created_on = models.DateField(
+        auto_now_add=True
+    )
+    closed = models.BooleanField()
+    closed_on = models.DateField(null=True)
+    entry = models.ForeignKey(
+        TrackingEntry
+    )
+    approver = models.ForeignKey(
+        Tbluser,
+        limit_choices_to={
+            "user_type__in": ["SUPER", "ADMIN"]
+        }
+    )
+
+    def close(self):
+        self.closed = True
+        self.closed_on = datetime.datetime.now()
+        self.save()
