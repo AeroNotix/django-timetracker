df61d4620606ae70d7ff42c07e25311165661667
AeroNotix <aaron.l.france@gmail.com>
2013-06-28 13:59:22 +0100

    Added the test module for the overtime page.

--- overtime/tests.py
+++ overtime/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import datetime
 
 from django.test import TestCase
+from django.core import mail
 
+from timetracker.overtime.models import PendingApproval
+from timetracker.tracker.models import TrackingEntry
+from timetracker.tests.basetests import create_users, delete_users
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ApprovalTest(TestCase):
+
+    def setUp(self):
+        create_users(self)
+        self.ot_entry = TrackingEntry(
+            user=self.linked_user,
+            entry_date=datetime.datetime.today(),
+            start_time=datetime.time(0, 0, 0),
+            end_time=datetime.time(17, 0, 0),
+            breaks=datetime.time(0, 15, 0),
+            daytype="WKDAY",
+        )
+        self.ot_entry.full_clean()
+        self.ot_entry.save()
+            
+    def tearDown(self):
+        delete_users(self)
+
+    def testPendingApprovalDenied(self):
+        self.doapprovaltest(False, "Request for Overtime: Denied.", 0)
+
+    def testPendingApprovalApproved(self):
+        self.doapprovaltest(True, "Your recent timetracker actions.", 1)
+
+    def doapprovaltest(self, status, message, attachments):
+        approval = PendingApproval(
+            entry=self.ot_entry,
+            approver=self.linked_manager
+        )
+        approval.save()
+        approval.close(status)
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, message)
+        self.assertEqual(len(mail.outbox[0].attachments), attachments)
